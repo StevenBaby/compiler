4bed22f6b72469600762265e00f89b5996f8765a
Steven Kang <kangweibaby@163.com>
2021-07-13 17:47:18 +0800

    add main

---
+++ src/main.py
@@ -0,0 +1,13 @@
+'''
+(C) Copyright 2021 Steven;
+@author: Steven kangweibaby@163.com
+@date: 2021-07-13
+'''
+
+
+def main():
+    print("This is a C compiler")
+
+
+if __name__ == '__main__':
+    main()
